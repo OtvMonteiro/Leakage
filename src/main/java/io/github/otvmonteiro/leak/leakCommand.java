0e8dfbc180426ff759d70218667284e605a0e28b
Otavio <ohmtudo@gmail.com>
2020-01-08 01:02:58 -0200

    Spawner Update

--- src/main/java/io/github/otvmonteiro/leak/leakCommand.java
+++ src/main/java/io/github/otvmonteiro/leak/leakCommand.java
@@ -4,11 +4,14 @@ package io.github.otvmonteiro.leak;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
 import org.bukkit.block.CreatureSpawner;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import java.util.Random;
 
@@ -26,35 +29,36 @@ public class leakCommand implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmnd, String alias, String[] args) {
 		//This command can only be executed by Players
 		if (!(sender instanceof Player)) {
+			Bukkit.broadcastMessage("This command can only be used by a player.");
 			return false;
 		}
 		//Cast the command sender to a Player
 		Player player = (Player) sender;
+		
 		//Get player location
 		Location loc = getRandomLocation(player.getLocation());
-		//Creates the SpawningBlock in the desired position
-	//	CreatureSpawner spawner = (CreatureSpawner) loc.getBlock();//APARENTEMENTE N POSSO FAZER ESSE CAST
-	//ERROR>> Caused by: java.lang.ClassCastException: org.bukkit.craftbukkit.v1_15_R1.block.CraftBlock cannot be cast to org.bukkit.block.CreatureSpawner
-
-		//spawner.setCreatureTypeByName("Cow");//
+		
+		//Create an explosion in the desired location
 		explodePlace(loc);
-		Block block = loc.getBlock();
-		block.setTypeId(52);
-		//spawner.setTypeId(92);
 		
+		//Creates the SpawningBlock in the desired position
+		createSpawner(loc);
+		//workingCreateSpawner(loc);
+		
+		//Broadcast it's appearance
 		Bukkit.broadcastMessage(ChatColor.RED+"Leakage appeard!!!");
-		
+	
 		return true;
 	}
 	
 	//Creates a random location, based on player's location
 	private Location getRandomLocation(Location p) {
 		//Get random addition (from -upperbound to +upperbound for X and Y and 0 to +upperbound for Z)
-		Random rand1 = new Random();int n1 = rand1.nextInt(getUpperbound()*2) - getUpperbound();
-		Random rand2 = new Random();int n2 = rand2.nextInt(getUpperbound()*2) - getUpperbound();
+		Random rand1 = new Random();int n1 = getUpperbound() - rand1.nextInt(getUpperbound()*2);
+		Random rand2 = new Random();int n2 = getUpperbound() - rand2.nextInt(getUpperbound()*2);
 		Random rand3 = new Random();int n3 = rand3.nextInt(getUpperbound()/2);//Not too high
 	
-		Location loc = new Location(p.getWorld(), p.getX()+n1, p.getY()+n2,p.getZ()+n3);
+		Location loc = new Location(p.getWorld(), p.getX()+n1, p.getY()+n3,p.getZ()+n2);//Y is height
 		return loc;
 	}
 	
@@ -63,6 +67,28 @@ public class leakCommand implements CommandExecutor {
         double y = loc.getY();
         double z = loc.getZ();
         loc.getWorld().createExplosion(x, y, z, 6, true, true);//Explode place, tnt power, setFire and BreakBlocks
+	}
+	
+	private void createSpawner (Location loc) {
+			
+			Block block = loc.getBlock();
+			block.setType(Material.MOB_SPAWNER);
+
+			CreatureSpawner spawner = (CreatureSpawner) block.getState();
+			spawner.setSpawnedType(EntityType.BAT);
+			spawner.setDelay(1);Bukkit.broadcastMessage(Integer.toString(spawner.getDelay()));
+			spawner.setRequiredPlayerRange(getUpperbound()*4);
+			spawner.setMaxNearbyEntities(50);
+			spawner.setSpawnRange(getUpperbound()*2);
+			spawner.update();			
+			
+			//spawner.setCreatureTypeByName("Cow");
+			
+	}
+	
+	private void workingCreateSpawner (Location loc) {//Deprecated
+			Block block = loc.getBlock();//These two lines works, but spawn only a pigSpawner,
+			block.setTypeId(52);		 //that is uncastable to CreatureSpawner	
 	}
 }
 
